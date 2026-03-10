76bd8066a1a8d9957ec5c3d253395b6353cec8dd
George-Zheng-1013 <Zhengche30@outlook.com>
2026-03-10 18:22:39 +0800

    删除旧streamlit文件

--- backend_engine.py
+++
@@ -1,7 +0,0 @@
-"""兼容入口：转发到 services/backend_engine.py。"""
-
-from services.backend_engine import main
-
-
-if __name__ == "__main__":
-    main()
--- ui/__init__.py
+++
@@ -1,1 +0,0 @@
-"""NetGuard frontend UI package."""
